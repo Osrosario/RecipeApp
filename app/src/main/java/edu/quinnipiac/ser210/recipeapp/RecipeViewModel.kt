741faabe549a486201ebbeb91857d03163550c23
Osrosario <osrosario1@gmail.com>
2023-04-26 10:44:27 -0400

    Upload Work

--- app/src/main/java/edu/quinnipiac/ser210/recipeapp/RecipeViewModel.kt
+++ app/src/main/java/edu/quinnipiac/ser210/recipeapp/RecipeViewModel.kt
@@ -15,26 +15,33 @@ import kotlinx.coroutines.launch
 
 class RecipeViewModel(val dao: RecipeDAO) : ViewModel()
 {
+    /** Initial variables replicating the columns of the Recipe data class */
     var newRecipeName = ""
     var newImageUrl = ""
     var newPrepTime = ""
     var newCookTime = ""
-    var newIngredients = ""
-    var newInstructions = ""
+    var newIngredientList = ArrayList<String>(8)
+    var newInstructionList = ArrayList<String>(8)
+
+    fun addIngredient(ingredient: String) = newIngredientList.add(ingredient)
+    fun addInstruction(instruction: String) = newInstructionList.add(instruction)
 
     fun addRecipe()
     {
         viewModelScope.launch {
+            /** Create a new recipe from the Recipe. */
             val recipe = Recipe()
 
+            /** Assign local variables to the data of the recipe. */
             recipe.recipeTitle = newRecipeName
             recipe.imageUrl = newImageUrl
             recipe.prepTime = newPrepTime
             recipe.cookTime = newCookTime
             recipe.totalTime = { newPrepTime.toInt() + newCookTime.toInt() }.toString()
-            recipe.ingredients = newIngredients
-            recipe.instructions = newInstructions
+            recipe.ingredients = newIngredientList.joinToString(separator = ".")
+            recipe.instructions = newInstructionList.joinToString(separator = ".")
 
+            /** Add new recipe to the database. */
             dao.insertRecipe(recipe)
         }
     }
